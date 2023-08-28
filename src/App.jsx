708712c986cc7a89f67daa25ce882014a99bdffb
Miqueias <miqueias.rihs@gmail.com>
2023-08-28 16:07:50 -0300

    CustomInput directory changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.scss';
 import React, { useEffect, useState } from 'react';
 
 import CustomForm from "./components/CustomForm/CustomForm"
-import CustomInput from './CustomInput/CustomInput';
+import CustomInput from './components/CustomInput/CustomInput';
 
 import { request } from './api/api';
 import CustomButton from './components/CustomButton/CustomButton';
@@ -34,39 +34,40 @@ export default function App() {
   };
 
   return (
-    // <div className='App-header'></div>
-    <div className="App-container">
-      <CustomForm>
-        <CustomInput
-          label="Nome:"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => handleInputChange(e, 'name')}
-          name="name"
-        />
-        <CustomInput
-          label="Documento:"
-          type="text"
-          placeholder="Digite seu CPF"
-          onChange={(e) => handleInputChange(e, 'document')}
-          name="document"
-        />
+    <>
+      <div className="App-container">
+        <CustomForm>
+          <CustomInput
+            label="Nome:"
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => handleInputChange(e, 'name')}
+            name="name"
+          />
+          <CustomInput
+            label="Documento:"
+            type="text"
+            placeholder="Digite seu CPF"
+            onChange={(e) => handleInputChange(e, 'document')}
+            name="document"
+          />
 
-        {inputList.map((f) => {
-          return (
-            <CustomInput
-              key={f.name}
-              label={`${f.label}:`}
-              type={f.options ? "select" : "text"}
-              placeholder={f.placeholder}
-              onChange={(e) => handleInputChange(e, f.name)}
-              name={f.name}
-              options={f.options ? f.options : null}
-            />
-          )
-        })}
-        <CustomButton onClick={handleSubmit}>Pedir análise</CustomButton>
-      </CustomForm>
-    </div>
+          {inputList.map((f) => {
+            return (
+              <CustomInput
+                key={f.name}
+                label={`${f.label}:`}
+                type={f.options ? "select" : "text"}
+                placeholder={f.placeholder}
+                onChange={(e) => handleInputChange(e, f.name)}
+                name={f.name}
+                options={f.options ? f.options : null}
+              />
+            )
+          })}
+          <CustomButton onClick={handleSubmit}>Pedir análise</CustomButton>
+        </CustomForm>
+      </div>
+    </>
   );
 }
--- src/components/CustomForm/CustomForm.jsx
+++ src/components/CustomForm/CustomForm.jsx
@@ -1,12 +1,10 @@
 import './CustomForm.scss'
 
-import CustomButton from '../CustomButton/CustomButton'
-
 export default function CustomForm({children, ...atributes}) {
 
     return (
         <div className='form-container'>
-            <h1>Análise de crédito</h1>
+            <h1>ANÁLISE DE CRÉDITO</h1>
             {children}
         </div>
     )
